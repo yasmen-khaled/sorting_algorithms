3215295e92ad63bc79b4b1912c1a1ebab8847fe1
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-20 23:35:45 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,37 @@
 #include "sort.h"
 
-/**
- * insertion_sort_list - Sort a linked list using insertion sort algorithm.
- *
- * @list: A pointer to list head.
- */
-
 void insertion_sort(listint_t **l)
 {
  listint_t *curr, *n;
 
  if (*l == NULL || (*l)->next == NULL)
-     return;
+    return;
 
  curr = (*l)->next;
  while (curr != NULL)
  {
-     n = curr;
-     while (n != *l && n->prev->n > n->n)
-     {
-         node_swap(l, &(n->prev), n);
-     }
-     curr = curr->next;
+    n = curr;
+    while (n != *l && n->prev->n > n->n)
+    {
+        node_swap(l, &(n->prev), n);
+    }
+    curr = curr->next;
  }
 }
-/**
- * node_swap - Swaps two nodes in a doubly linked list.
- * @l: A pointer to list head.
- * @curr: A pointer to first node.
- * @n: The second node.
- */
+
 void node_swap(listint_t **l, listint_t **curr, listint_t *n)
 {
  (*curr)->next = n->next;
  if (n->next != NULL)
-     n->next->prev = *curr;
+    n->next->prev = *curr;
 
  n->prev = (*curr)->prev;
  n->next = *curr;
 
  if ((*curr)->prev != NULL)
-     (*curr)->prev->next = n;
+    (*curr)->prev->next = n;
  else
-     *l = n;
+    *l = n;
 
  (*curr)->prev = n;
  *curr = n->prev;
