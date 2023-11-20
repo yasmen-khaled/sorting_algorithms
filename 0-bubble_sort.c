b3cc8dd98921055b16118ad7aa38c30f46ae5a35
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-20 22:10:24 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,27 @@
-#include "sort.h"
+#include <stdio.h>
 
-/**
- * swap - Swaps
- * @x:pointer.
- * @y:pointer.
- *
- * Return: Nothing.
- */
-void swap(int *x, int *y)
-{
-	int temp = *x;
-
-	*x = *y;
-	*y = temp;
-}
-
-/**
- * bubble_sort - Sorts an array
- * @array:pointer
- * @size:size of the array.
- * Return: nothing.
- */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
-	bool swapped;
+   size_t i, j;
+   int swapped;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = false;
+   for (i = 0; i < size - 1; i++)
+   {
+       swapped = 0;
 
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				swapped = true;
-			}
-		}
+       for (j = 0; j < size - i - 1; j++)
+       {
+           if (array[j] > array[j + 1])
+           {
+               int temp = array[j];
+               array[j] = array[j + 1];
+               array[j + 1] = temp;
 
-		if (!swapped)
-			break;
-	}
+               swapped = 1;
+           }
+       }
+
+       if (!swapped)
+           break;
+   }
 }
