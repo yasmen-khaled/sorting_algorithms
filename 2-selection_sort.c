2f6bbe44b60b34ec2624cea6cb2d7f064d1660f7
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-21 13:57:33 +0300

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array
+ * @array: array
+ * @size: size of the array
+ *
+ * Return:func
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t outer_loop, inner_loop;
+	size_t smallest_index;
+
+	for (outer_loop = 0; outer_loop < size - 1; outer_loop++)
+	{
+		smallest_index = outer_loop;
+
+		for (inner_loop = outer_loop + 1; inner_loop < size; inner_loop++)
+		{
+			if (array[inner_loop] < array[smallest_index])
+				smallest_index = inner_loop;
+		}
+
+		if (outer_loop != smallest_index)
+		{
+			swap(&array[outer_loop], &array[smallest_index]);
+			print_array((const int *)array, size);
+		}
+	}
+}
+
+
+/**
+ * swap - Swaps
+ * @x:pointer
+ * @y:pointer
+ * Return:non
+ */
+void swap(int *xd, int *yd)
+{
+	int temp = *xd;
+
+	*xd = *yd;
+	*yd = temp;
+}
