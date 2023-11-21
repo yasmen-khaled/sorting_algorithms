09d72674c29f11b54c4a896aae6bd34a42009117
yasmen <yasmen.khaled2000@gmail.com>
2023-11-22 01:39:29 +0200

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,39 +2,39 @@
 
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *current, *next, *insert;
+	listint_t *current, *next, *insert;
 
-  if (!list || !(*list) || !((*list)->next))
-      return;
+	if (!list || !(*list) || !((*list)->next))
+		return;
 
-  for (current = (*list)->next; current != NULL; current = next)
-  {
-      next = current->next;
-      insert = current->prev;
+	for (current = (*list)->next; current != NULL; current = next)
+	{
+		next = current->next;
+		insert = current->prev;
 
-      while (insert != NULL && current->n < insert->n)
-      {
-          node_swap(list, &insert, current);
-          print_list((const listint_t *)*list);
-      }
-  }
+		while (insert != NULL && current->n < insert->n)
+	{
+		node_swap(list, &insert, current);
+		print_list((const listint_t *)*list);
+		}
+	}
 }
 
 
 void node_swap(listint_t **j, listint_t **c, listint_t *x)
 {
- (*c)->next = x->next;
- if (x->next != NULL)
-    x->next->prev = *c;
+	(*c)->next = x->next;
+	if (x->next != NULL)
+	x->next->prev = *c;
 
- x->prev = (*c)->prev;
- x->next = *c;
+	x->prev = (*c)->prev;
+	x->next = *c;
 
- if ((*c)->prev != NULL)
-    (*c)->prev->next = x;
- else
-    *j = x;
+	if ((*c)->prev != NULL)
+	(*c)->prev->next = x;
+	else
+	*j = x;
 
- (*c)->prev = x;
- *c = x->prev;
+	(*c)->prev = x;
+	*c = x->prev;
 }
