ba3032bfe256e1c0290ab8a56537b0624ab6c256
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-22 00:09:25 +0300

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,1 +1,83 @@
+#include "sort.h"
 
+/**
+ * quick_sort - quick sort algorithm
+ * @arrayToSort:pointer
+ * @arraySize: size of the array.
+ * Return: non
+ */
+void quick_sort(int *arrayToSort, size_t arraySize)
+{
+	if (arraySize < 2)
+		return;
+
+	Qsort(arrayToSort, 0, (int)arraySize - 1, arraySize);
+}
+
+/**
+ * recursive - Manage
+ * @arrayToSort:pointer to the array to sort.
+ * @startIndex: The start position.
+ * @endIndex: The end position.
+ * @arraySize:size of the array.
+ */
+
+void recursive(int *arrayToSort, int startIndex, int endIndex, size_t arraySize)
+{
+	int pivotIndex;
+
+	if (startIndex < endIndex)
+	{
+		pivotIndex = lumoto_part(arrayToSort, startIndex, endIndex, arraySize);
+
+		Qsort(arrayToSort, startIndex, pivotIndex - 1, arraySize);
+		Qsort(arrayToSort, pivotIndex + 1, endIndex, arraySize);
+	}
+}
+
+/**
+ * parti - Handles partioning
+ * @arrayToSort:pointer
+ * @startIndex:position.
+ * @endIndex:position.
+ * @arraySize:array.
+ * Return:index.
+ */
+
+int parti(int *arrayToSort, int startIndex, int endIndex, size_t arraySize)
+{
+	int pivot = arrayToSort[endIndex];
+	int index = startIndex, x, allElementsEqual = 1;
+
+	for (x = startIndex; x < endIndex; x++)
+	{
+		if (arrayToSort[x] != pivot)
+			allElementsEqual = 0;
+
+		if (arrayToSort[x] < pivot)
+		{
+			swap(&arrayToSort[index], &arrayToSort[x]);
+			if (index != x && !allElementsEqual)
+				print_array(arrayToSort, arraySize);
+			index++;
+		}
+	}
+	swap(&arrayToSort[index], &arrayToSort[endIndex]);
+	if (index != x && !allElementsEqual)
+		print_array(arrayToSort, arraySize);
+
+	return (index);
+}
+/**
+ * swap - Swaps
+ * @x:pointer.
+ * @y:pointer.
+ *Return: non
+ */
+void swap(int *firstElement, int *secondElement)
+{
+	int temp = *firstElement;
+
+	*firstElement = *secondElement;
+	*secondElement = temp;
+}
