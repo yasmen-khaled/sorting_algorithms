a399b8ff749a3ea8ce7e5d4ee04c26857c22634c
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-22 00:09:49 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort(int *arrayToSort, size_t arraySize);
 #endif
